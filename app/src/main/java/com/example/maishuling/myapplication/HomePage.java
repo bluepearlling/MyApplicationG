f14a7de423cd236d7e838644660df6bcc5f0bacc
bluepearlling <maisurahshuling@gmail.com>
2018-07-29 23:34:48 +0800

    First Comit

---
+++ app/src/main/java/com/example/maishuling/myapplication/HomePage.java
@@ -0,0 +1,18 @@
+package com.example.maishuling.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        //Intent intent = getIntent();
+
+
+    }
+}
