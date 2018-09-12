5607fb32ed01a7f1a5fde4c96365af1bfdc6bd90
bluepearlling <maisurahshuling@gmail.com>
2018-09-12 19:40:47 +0800

    android app with working WS connection

--- app/src/main/java/com/example/maishuling/myapplication/MainActivity.java
+++ app/src/main/java/com/example/maishuling/myapplication/MainActivity.java
@@ -1,22 +1,37 @@
 package com.example.maishuling.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button LoginButton = (Button) findViewById(R.id.loginButton);
 
-        LoginButton.setOnClickListener(new View.OnClickListener() {
-                                           public void onClick(View view) {
+        LoginButton.setOnClickListener(new View.OnClickListener()
+                                       {
+                                           public void onClick(View view)
+                                           {
                                                Intent i = new Intent(MainActivity.this, HomePage.class);
                                                startActivity(i);
                                            }
@@ -24,4 +39,5 @@ public class MainActivity extends AppCompatActivity {
                                        }
         );
     }
+
 }
